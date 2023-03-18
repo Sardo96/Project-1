05ad41279e3d11eeb3237bbba7fe868540616255
Miguel Barroca Sardo <miguel.sardo@constru.pt>
2023-03-18 16:31:42 +0000

    18_03_2023 Update

---
+++ scripts/input.js
@@ -0,0 +1,25 @@
+export class InputHandler {
+    constructor() {
+        this.keys = []; 
+        window.addEventListener('keydown', e => {
+            if ((   e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp'   ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight'||
+                    e.key === 'q'
+                ) && this.keys.indexOf(e.key) === -1) {
+                this.keys.push(e.key);
+            }
+        });
+        window.addEventListener('keyup', e => {
+           if (     e.key === 'ArrowDown' || 
+                    e.key === 'ArrowUp'   ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight'||
+                    e.key === 'q'
+                    ) {
+            this.keys.splice(this.keys.indexOf(e.key), 1);
+           }  
+        });
+    }
+}
